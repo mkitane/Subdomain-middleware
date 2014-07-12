1bedd9bce723918da93e7e2480ff12fe657308c1
mkitane <mehdikitane@gmail.com>
2014-07-12 19:17:47 +0200

    add support of localhost

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // the middleware function
 module.exports = function() {
     return function(req, res, next) {
-		var array = /(.*)\.(?:.*)\.(?:.*)/i.exec(req.host);
+    	console.log("HOST : " + req.host);
 
+		var array = /(.*)\.(?:(?:.*)\.(?:.*)|localhost)/i.exec(req.host);
+		console.log(array);
 		if(array != null)
 		{
 			var subdomain = array[1];
-			req.url = subdomain + '/' + req.url; 
+			req.url = '/' + subdomain + '/' + req.url; 
 		}
     	next();
     }
